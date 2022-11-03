d445c26b38c4a58bd69e42c001124e8cb62e18cc
Rupesh Duwal <rupeshduwal17@gmail.com>
2022-11-03 08:22:19 +0545

    Modified

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import chalk from 'chalk';
 // console.log(chalk.yellow('Hello World'));
-console.log(chalk.yellow('Hello'),  chalk.blue('World'));
+// console.log(chalk.yellow('Hello'),  chalk.blue('World'));
+console.log(chalk.blue('Hello'), chalk.yellow('World'), chalk.red('!'))
